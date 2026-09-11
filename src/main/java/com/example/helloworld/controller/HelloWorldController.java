11a91663f899462379709a878789ba7ab7d1ea5f
Genaro19710920 <valter.yokoyama@gmail.com>
2026-09-11 14:30:31 -0300

    Incluido variavel de ambiente

--- src/main/java/com/example/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/example/helloworld/controller/HelloWorldController.java
@@ -1,13 +1,16 @@
 package com.example.helloworld.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
+    @Value("${app.data}")
+    private String data;
     @GetMapping("/hello")
     public String sendGreetings() {
-        return "Hello, World!!";
+        return "Hello, World!!" +data;
     }
 }
