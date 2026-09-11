1845f2c04b2fba8f35ad61329f41432cbde317ea
Genaro19710920 <valter.yokoyama@gmail.com>
2026-09-11 16:43:17 -0300

    no message

--- src/main/java/com/example/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/example/helloworld/controller/HelloWorldController.java
@@ -11,6 +11,6 @@ public class HelloWorldController {
     private String data;
     @GetMapping("/hello")
     public String sendGreetings() {
-        return "Hello, New World teste!!" +data;
+        return "Hello everybody!!" +data;
     }
 }
